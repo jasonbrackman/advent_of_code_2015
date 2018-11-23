9c47594a0ed273b352b4d58106a453cda1d8e2d3
jasonbrackman <brackman@gmail.com>
2018-11-23 12:32:29 -0800

    simplified some of the trim() calls.

--- src/day_07.rs
+++ src/day_07.rs
@@ -19,36 +19,34 @@ impl Circuit {
     pub fn parse_line(&mut self, input: &str) -> i32 {
         let mut problems = 0;
 
-        let args = input.split("->").collect::<Vec<&str>>();
+        let mut args = input.split_whitespace().map(str::trim).collect::<Vec<&str>>();
 
-        // ensure rhs is in hashmap with a default zero value if not.
-        let rhs: &str = args[1];
-
-        // break down the lhs into its args (1 to 3 expected)
-        let lhs: Vec<&str> = args[0].split_whitespace().collect();
+        let rhs = args.pop().unwrap();
+        let _ = args.pop(); // remove the ->
 
 
-        if lhs.len() == 1 { // must be a number!
-            match lhs[0].parse::<u16>() {
-                Ok(n) => *self.registers.entry(rhs.trim().to_string()).or_insert(0) = n,
-                Err(_) => if self.registers.contains_key(lhs[0].trim()) {
+        if args.len() == 1 {
+
+            match args[0].parse::<u16>() {
+                Ok(n) => *self.registers.entry(rhs.to_string()).or_insert(0) = n,
+                Err(_) => if self.registers.contains_key(args[0]) {
                     *self.registers
-                        .entry(rhs.trim().to_string())
-                        .or_insert(0) = self.registers[lhs[0].trim()];
+                        .entry(rhs.to_string())
+                        .or_insert(0) = self.registers[args[0]];
                 } else {
                     problems += 1
                 }
             };
 
-        } else if lhs.len() == 2 {
+        } else if args.len() == 2 {
 
-            match lhs[1].parse::<u16>() {
+            match args[1].parse::<u16>() {
                 Ok(n) => *self.registers.entry(rhs.trim().to_string()).or_insert(0) = !n,
                 Err(_) => {
-                    if self.registers.contains_key(lhs[1].trim()) {
+                    if self.registers.contains_key(args[1]) {
                         *self.registers
-                            .entry(rhs.trim().to_string())
-                            .or_insert(0) = !self.registers[lhs[1].trim()];
+                            .entry(rhs.to_string())
+                            .or_insert(0) = !self.registers[args[1]];
                     } else {
                         problems += 1;
                     }
@@ -56,27 +54,30 @@ impl Circuit {
             };
 
         } else {
-            let arg_1 = lhs[0].parse::<u16>();
-            let arg_2 = lhs[2].parse::<u16>();
-
+            let arg_1 = args[0].parse::<u16>();
             let mut result1 = 0;
             if arg_1.is_ok() {
                 result1 = arg_1.unwrap();
-            } else if self.registers.contains_key(lhs[0].trim()) {
-                result1 = self.registers[lhs[0].trim()];
-            } else { problems += 1;}
+            } else if self.registers.contains_key(args[0]) {
+                result1 = self.registers[args[0]];
+            } else {
+                problems += 1;
+            }
 
+            let arg_2 = args[2].parse::<u16>();
             let mut result2 = 0;
             if arg_2.is_ok() {
                 result2 = arg_2.unwrap();
-            } else if self.registers.contains_key(lhs[2].trim()) {
-                result2 = self.registers[lhs[2].trim()];
+            } else if self.registers.contains_key(args[2]) {
+                result2 = self.registers[args[2]];
             } else {
                 problems += 1;
             }
 
             if problems == 0 {
-                *self.registers.entry(rhs.trim().to_string()).or_insert(0) = match lhs[1].trim() {
+                *self.registers
+                    .entry(rhs.to_string())
+                    .or_insert(0) = match args[1] {
                     "AND" => result1 & result2,
                     "OR" => result1 | result2,
                     "LSHIFT" => result1 << result2,
@@ -122,10 +123,12 @@ fn test_get_parent_values() {
     let mut total = -1;
     while total != 0 {
         total = 0;
-        total += circuit.parse_line("NOT eo -> z");
+        total += circuit.parse_line("124 -> f");
+        total += circuit.parse_line("eo AND f -> gg");
+        total += circuit.parse_line("gg OR eo -> z");
         total += circuit.parse_line("9 -> eo");
 
     }
-    println!("total: {} -> z: {}", total, circuit.registers["z"]);
+    assert_eq!(circuit.registers["z"], 9);
 
 }
