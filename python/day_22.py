1432d185a0e3b1d68355ec0e35ba779784784f35
jasonbrackman <brackman@gmail.com>
2019-10-03 07:19:11 -0700

    so.much.state -- still working on it.

--- python/day_22.py
+++ python/day_22.py
@@ -22,24 +22,27 @@
 
 from __future__ import annotations
 
+import copy
 from collections import deque
-from typing import NamedTuple, Tuple, Optional, Set, List
+from typing import Optional, Set, List
 
 
-class Effect(NamedTuple):
-    name: str
-    turns: int
-    armor: int
-    damage: int
-    mana: int
+class Effect:
+    def __init__(self, name:str, turns: int, armor: int, damage: int, mana: int) -> None:
+        self.name: str = name
+        self.turns: int = turns
+        self.armor: int = armor
+        self.damage: int = damage
+        self.mana: int = mana
 
 
-class Spell(NamedTuple):
-    name: str
-    cost: int
-    damage: int
-    heal: int
-    effect: Optional[Tuple]
+class Spell:
+    def __init__(self, name: str, cost: int, damage: int, heal: int, effect: Optional[Effect]) -> None:
+        self.name: str = name
+        self.cost: int = cost
+        self.damage: int = damage
+        self.heal: int = heal
+        self.effect: Optional[Effect] = effect
 
 
 class Character:
@@ -60,18 +63,69 @@ class GameState:
         self.player = player
         self.spells = spells
 
+        self.is_player_turn_over: bool = False
+
     def __str__(self):
         text = [
             f"{s.effect.name} deals {s.effect.damage}; its timer is now {s.effect.turns}"
-            for s in self.boss.spells
+            for s in self.player.spells if s.effect is not None
         ]
 
+        turn_owner = "Player" if self.is_player_turn_over else "Boss"
+
+        msg = f"Boss attacks for {self.boss.damage - self.player.armor} damage.\n"
+        if self.is_player_turn_over:
+            # Must have finished a round where it was the player's turn
+            msg = f"Player attacks for {self.player.damage - self.boss.armor} damage.\n"
+
         return (
-            f" - Player has {self.player.hit_points} hit points, {self.player.armor} armor, {self.player.mana} mana",
-            f" - Boss has {self.boss.hit_points} hit points",
-            *text,
-            f"Boss attacks for {self.boss.damage - self.player.armor} damage.",
+            f"-- {turn_owner} turn --\n"
+            f" - Player has {self.player.hit_points} hit points, {self.player.armor} armor, {self.player.mana} mana\n"
+            f" - Boss has {self.boss.hit_points} hit points\n"
+            f"{text}\n"
+            f"{msg}\n"
+
         )
+
+    def take_turn(self, spell=None):
+        self.cast_active_spell_effects()
+        if spell is not None:
+            self.cast_new_spell(spell)
+
+        self.is_player_turn_over = not self.is_player_turn_over
+
+    def cast_active_spell_effects(self):
+        to_remove = list()
+
+        # Deal all spell effects that are active each turn
+        for s in self.player.spells:
+            if hasattr(s, "effect") and s.effect is not None:
+                # poison
+                self.boss.hit_points -= s.effect.damage - self.boss.armor
+                # Recharge
+                self.player.mana += s.effect.mana
+
+                # Reduce the turns
+                s.effect.turns -= 1
+                if s.effect.turns <= 0:
+                    # Remove one time active boosts from effects
+                    self.player.armor -= s.effect.armor
+                    to_remove.append(s)
+                    # self.player.spells.remove(s)
+
+        for d in to_remove:
+            self.player.spells.remove(d)
+
+    def cast_new_spell(self, spell: Spell):
+        if not self.is_player_turn_over:
+            self.player.mana -= spell.cost
+            # Check if its an instant spell or has an effect
+            if spell.effect is None:  # instant
+                self.boss.hit_points -= spell.damage - self.boss.armor
+            else:
+                # Add the one time armor bonus -- will be removed when spell is deleted
+                self.player.armor += spell.effect.armor
+                self.player.spells.add(spell)
 
     def goal_test(self) -> bool:
         if self.boss.hit_points <= 0:
@@ -87,16 +141,27 @@ class GameState:
 
         return True
 
-    def successors(self) -> List[Optional[Spell]]:
-        items: List[Spell] = []
-        for spell in self.spells:
-            if spell.cost <= self.player.mana:
+    def successors(self) -> List[Optional[GameState]]:
+        items: List[GameState] = []
 
-                for boss_spells in self.boss.spells:
-                    if boss_spells.effect.turns <= 1:
-                        continue
+        if self.is_player_turn_over:
+            # Boss Turn
+            temp = copy.deepcopy(self)
+            temp.take_turn()
+            temp.player.hit_points -= temp.boss.damage - temp.player.armor
+            if temp.is_legal():
+                items.append(temp)
 
-                items.append(spell)
+        else:
+            for spell in self.spells:
+                # Can't reuse an existing active spell
+                if all(spell.name != s.name for s in self.player.spells):
+                    # must be affordable
+                    if spell.cost <= self.player.mana:
+                        temp = copy.deepcopy(self)
+                        temp.take_turn(spell=spell)
+                        if temp.is_legal():
+                            items.append(temp)
 
         return items
 
@@ -131,17 +196,24 @@ def main():
     player = Character("Player", hit_points=50, mana=500, damage=0, armor=0)
 
     game = GameState(boss=boss, player=player, spells=spells)
-    for itm in game.successors():
-        print(itm)
+    solution = bfs(game, game.goal_test, game.successors)
+    if solution is not None:
+        while solution.parent:
+            print(solution.state)
+            solution = solution.parent
+    else:
+        print("No solution found.")
+    # for itm in game.successors():
+    #     print(itm)
 
 
 class Node:
-    def __init__(self, spell: Spell, node: Optional[Node]):
-        self.state = spell
+    def __init__(self, state: GameState, node: Optional[Node]):
+        self.state = state
         self.parent = node
 
 
-def bfs(initial, goal_test, successors):
+def bfs(initial: GameState, goal_test, successors) -> Optional[List[Spell]]:
     # frontier is where we've yet to go
     frontier = deque()
     frontier.append(Node(initial, None))  # explored is where we've been
@@ -152,10 +224,10 @@ def bfs(initial, goal_test, successors):
     while frontier:
         current_node = frontier.pop()
         current_state = current_node.state  # if we found the goal, we're done
-        if goal_test(current_state):
+        if current_state.goal_test():
             return current_node
         # check where we can go next and haven't explored
-        for child in successors(current_state):
+        for child in current_state.successors():
             if child in explored:  # skip children we already explored
                 continue
             explored.add(child)
